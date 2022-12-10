fb09a6636a689d5fdd2fc8a9c93453aa8427fa4a
J Patrick McKeown <jpmckeown@users.noreply.github.com>
2022-12-10 14:49:01 +0000

    GFN testing API

---
+++ gfn1.py
@@ -0,0 +1,26 @@
+password = "1Bv9rtTR2b57O9PtaoH2CHimi6KDq9gk53SM552O12qNSBN6a7IK"
+user = "any"
+endpoint = "https://api.footprintnetwork.org/v1/types"
+
+import requests
+# auth = 
+# response = requests.get(endpoint).json()
+
+# CURL approach fails
+# import pycurl
+# c = pycurl.Curl()
+
+# # Initialize the session
+# session = curl_init(endpoint)
+
+# # Set curl options 
+# c.setopt(session, CURLOPT_HEADER, true)
+# c.setopt(session, CURLOPT_RETURNTRANSFER, true)
+# c.setopt(session, CURLOPT_HTTPAUTH, CURLAUTH_BASIC)
+# c.setopt(session, CURLOPT_USERPWD, "user:password")
+
+# c.setopt(c.URL, endpoint)
+# c.perform()
+# response = c.exec(session)
+# c.end()
+
