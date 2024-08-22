9a08c6896ea231b0253261c6ed8a3471d25aae9d
J Patrick McKeown <jpmckeown@users.noreply.github.com>
2024-08-22 09:06:57 +0100

    stem of country name, number of photos, ISO codes 2 & 3, and continent imported unchanged from 2022

---
+++ gfn_2024.py
@@ -0,0 +1,27 @@
+password = "1CuOfc5PHG987b2Q8plMtmQD2CEA9E5BSvb9p9eNRe8S1CEcmKpr"
+user = "any"
+url = "https://api.footprintnetwork.org/v1/countries"
+
+import requests
+
+# auth = 
+# response = requests.get(endpoint).json()
+
+# CURL approach fails
+# import pycurl
+# c = pycurl.Curl()
+
+# # Initialize the session
+# session = curl_init(endpoint)
+
+# # Set curl options 
+# c.setopt(session, CURLOPT_HEADER, true)
+# c.setopt(session, CURLOPT_RETURNTRANSFER, true)
+# c.setopt(session, CURLOPT_HTTPAUTH, CURLAUTH_BASIC)
+# c.setopt(session, CURLOPT_USERPWD, "user:password")
+
+# c.setopt(c.URL, endpoint)
+# c.perform()
+# response = c.exec(session)
+# c.end()
+
